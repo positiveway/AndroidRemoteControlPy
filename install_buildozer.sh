cbee8a56e0f373b17b62c1e389cd5ceccf764153
user <PositiveWay05@gmail.com>
2021-11-02 14:06:10 +0300

    install command

--- install_buildozer.sh
+++ install_buildozer.sh
@@ -1,4 +1,4 @@
-sudo apt install -y git zip unzip default-jdk python3-pip autoconf libtool pkg-config zlib1g-dev cmake libffi-dev libssl-dev debhelper python3-dev
+sudo apt install -y git zip unzip default-jdk python3-pip python3-setuptools autoconf libtool pkg-config zlib1g-dev cmake libffi-dev libssl-dev debhelper python3-dev
 sudo pip install cython
 sudo pip install kivy
 sudo pip install buildozer
