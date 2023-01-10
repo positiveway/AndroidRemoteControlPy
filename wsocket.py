e784b8aea433e6c95d67f9a262fe55fa7c7c1037
andrew <PositiveWay05@gmail.com>
2023-01-10 14:44:08 +0500

    over phone

--- wsocket.py
+++ wsocket.py
@@ -1,7 +1,14 @@
 import socket
 
-server_ip_num = 104
-server_ip = f'192.168.1.{server_ip_num}'
+connected_over_phone = False
+
+if connected_over_phone:
+    server_ip_num = 200
+    server_ip = f'192.168.161.{server_ip_num}'
+else:
+    server_ip_num = 104
+    server_ip = f'192.168.1.{server_ip_num}'
+
 server_port = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
