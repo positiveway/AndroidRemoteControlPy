85deda64147aeaa47c97bc725ceb25d999271fce
andrew <PositiveWay05@gmail.com>
2023-01-19 12:32:21 +0500

    game layout

--- game_layout.py
+++ game_layout.py
@@ -27,10 +27,18 @@ def make_buttons(app):
 def fill_layout(app):
     app.root = GridLayout(cols=2, rows=1)
 
-    wasd_layout = GridLayout()
+    wasd_layout = GridLayout(cols=3, rows=2)
+    wasd_layout.add_widget(Label())
+    wasd_layout.add_widget(app.w_btn)
+    wasd_layout.add_widget(Label())
+    wasd_layout.add_widget(app.a_btn)
+    wasd_layout.add_widget(app.s_btn)
+    wasd_layout.add_widget(app.d_btn)
+
+    upper_left_side = GridLayout(cols=3, rows=3)
 
     left_side = GridLayout(cols=1, rows=2)
-    left_side.add_widget(Label())
+    left_side.add_widget(wasd_layout)
     left_side.add_widget(Label())
 
     right_side = GridLayout(cols=1, rows=2)
