e838cb42faf68d32e69465ccd9249d3cd5610b1c
andrew <PositiveWay05@gmail.com>
2023-01-20 23:26:47 +0500

    game layout

--- game_layout.py
+++ game_layout.py
@@ -32,6 +32,10 @@ def make_buttons(app):
         "Space", app,
         buttons="Space"
     )
+    app.shift_btn = UniversalButton(
+        "Shift", app,
+        button_codes="Caps"
+    )
 
 
 def fill_layout(app):
@@ -46,14 +50,27 @@ def fill_layout(app):
     wasd_layout.add(2, 3, app.d_btn)
     wasd_layout.fill()
 
+    esc_layout = Layout(cols=2, rows=2)
+    esc_layout.add(1, 2, app.esc_btn)
+    esc_layout.fill()
+
+    shift_layout = Layout(cols=1, rows=2)
+    shift_layout.add(1, 1, app.shift_btn)
+    shift_layout.fill()
+
     upper_left_side = Layout(cols=2, rows=2)
-    upper_left_side.add(1, 1, app.space_btn)
-    upper_left_side.add(1, 2, app.esc_btn)
+    upper_left_side.add(2, 2, app.space_btn)
+    upper_left_side.add(1, 2, esc_layout)
     upper_left_side.add(2, 1, wasd_layout)
+    upper_left_side.add(1, 1, shift_layout)
     upper_left_side.fill()
 
+    release_all_layout = Layout(cols=2, rows=2)
+    release_all_layout.add(2, 2, app.release_all_btn)
+    release_all_layout.fill()
+
     lower_left_side = Layout(cols=3, rows=3)
-    lower_left_side.add(3, 1, app.release_all_btn)
+    lower_left_side.add(3, 3, release_all_layout)
     lower_left_side.fill()
 
     left_side = Layout(cols=1, rows=2)
