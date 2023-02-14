5dcafac70f03cabeac0e6483d3487cdaacb187e8
andrew <PositiveWay05@gmail.com>
2023-02-14 16:04:07 +0500

    fixes

--- touchpad.py
+++ touchpad.py
@@ -35,15 +35,14 @@ class TouchpadWidget(Widget):
             self.prev_y = round(touch_event.y)
 
             self.touch_down_count += 1
+            if self.touch_down_count > 2:
+                raise ValueError(self.touch_down_count)
 
             if touch_event.is_double_tap:
                 if self.controller.is_mouse_mode:
                     self.double_tap_func()
                 # else:
                 #     self.controller.is_mouse_mode = True
-            else:
-                if self.controller.is_mouse_mode:
-                    pass
 
             self.clear_typed_text()
             return True
@@ -108,7 +107,7 @@ class TouchpadWidget(Widget):
             else:
                 self.send_if_not_empty()
 
-            self.draw_touch(touch_event)
+            # self.draw_touch(touch_event)
             return True
         else:
             # don't reset here it will prevent releasing pressed button on touch_up
@@ -116,7 +115,8 @@ class TouchpadWidget(Widget):
             return super().on_touch_move(touch_event)
 
     def on_touch_up(self, touch_event):
-        if self.is_in_zone(touch_event):
+        in_zone = self.is_in_zone(touch_event)
+        if in_zone:
             if self.touch_down_count == 2:
                 self.two_fingers_func()
 
@@ -127,8 +127,8 @@ class TouchpadWidget(Widget):
             self.controller.send_released(self.controller.LeftMouse)
             gc.collect()
 
-        if self.is_in_zone(touch_event):
-            self.clear_canvas()
+        if in_zone:
+            # self.clear_canvas()
             return True
         else:
             return super().on_touch_up(touch_event)
