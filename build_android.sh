9ed70621eccc0404f14aacccb0cf0943d9532c46
andrew <PositiveWay05@gmail.com>
2023-05-30 23:16:16 +0500

    android build

--- build_android.sh
+++ build_android.sh
@@ -1,3 +1,6 @@
+# exit when any command fails
+set -e
+
 # ndk bundle will only work with java 8 as default system-wide
 sudo apt-get install openjdk-8-jre -y
 sudo apt-get install openjdk-8-jdk -y
@@ -7,18 +10,28 @@ sdk_dir="$HOME/Android/Sdk"
 export PATH=$PATH:$sdk_dir/platform-tools
 export PATH=$PATH:$sdk_dir/emulator
 export PATH=$PATH:$sdk_dir/tools/bin
+
+# https://developer.android.com/ndk/downloads/
+# place content of the folder in $sdk_dir/ndk-bundle
+
 sdkmanager ndk-bundle
 export PATH=$PATH:$sdk_dir/ndk-bundle
 
+# https://github.com/kivy/python-for-android/blob/0.7.0/pythonforandroid/recipes/libffi/__init__.py#L13
+sudo apt install automake autoconf libltdl-dev ant ccache cmake gcc git libncurses libtool libssl-dev patch unzip zlib zip
+pip install cython virtualenv python-for-android
+
 # https://stackoverflow.com/questions/60151351/what-is-the-proper-way-to-get-android-ndk-version
 # https://developer.android.com/ndk/downloads/revision_history
+# https://github.com/kivy/python-for-android/issues/2685
 sdk_version="33"
 
-#sdkmanager "platforms;android-$sdk_version"
-export ANDROIDSDK="$sdk_dir/platforms/android-$sdk_version"
+sdkmanager "platforms;android-$sdk_version"
+export ANDROIDSDK="$sdk_dir"
 export ANDROIDNDK="$sdk_dir/ndk-bundle"
 export ANDROIDAPI=$sdk_version  # Target API version of your application
 export NDKAPI=$sdk_version  # Minimum supported API version of your application
-export ANDROIDNDKVER="r22b"  # Version of the NDK you installed
 
-p4a apk --private $HOME/PycharmProjects/AndroidRemoteControlPy --package=org.positiveway.controllerpython --name "Controller Python" --version 0.1 --bootstrap=sdl2 --requirements=python3,kivy
+echo $ANDROIDSDK
+
+p4a apk --private $HOME/PycharmProjects/AndroidRemoteControlPy --package=org.positiveway.controllerpython --name "Controller Python" --version 0.1 --bootstrap=sdl2 --requirements=python3,kivy --arch=arm64-v8a
